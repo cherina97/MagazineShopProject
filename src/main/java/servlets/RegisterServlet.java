d82dae66518694e2021198a49d3a14b15a9f9f3c
Cherina97 <Cherina97@gmail.com>
2020-03-22 17:28:05 +0200

    add cabinet files

---
+++ src/main/java/servlets/RegisterServlet.java
@@ -0,0 +1,41 @@
+package servlets;
+
+import entities.User;
+import entities.UserRole;
+import org.apache.commons.lang3.ObjectUtils;
+import services.UserService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "RegisterServlet", urlPatterns = {"/register"})
+public class RegisterServlet extends HttpServlet {
+    private UserService userService = UserService.getInstance();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        //todo user by default
+        if (ObjectUtils.allNotNull(firstName, lastName, email, password)) {
+            userService.create(new User.Builder()
+                    .withFirstName(firstName)
+                    .withLastName(lastName)
+                    .withEmail(email)
+                    .withRole(UserRole.USER.toString())
+                    .withPassword(password)
+                    .build());
+            resp.setStatus(HttpServletResponse.SC_CREATED);
+            return;
+        }
+        resp.setContentType("text/plain");
+        resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+    }
+}
