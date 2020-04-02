439d4dbcbaddae7b3e9ce8bc1bd98da230237c1c
Cherina97 <Cherina97@gmail.com>
2020-04-02 21:14:37 +0300

    add jpa entities

---
+++ src/main/java/resources/EntityManagerUtils.java
@@ -0,0 +1,22 @@
+package recources;
+
+import services.BucketService;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerUtils {
+    private static EntityManagerFactory entityManagerFactory;
+
+    public static EntityManagerFactory getEntityManagerFactory(){
+        if (entityManagerFactory == null){
+            entityManagerFactory = Persistence.createEntityManagerFactory("MagazineShopPersistence");
+        }
+        return entityManagerFactory;
+    }
+
+    public static EntityManager getEntityManager(){
+        return getEntityManagerFactory().createEntityManager();
+    }
+}
